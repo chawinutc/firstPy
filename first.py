7964893a346e8ba52df1d1d9bd95548ae6fc1b75
chawinutc <chawin@utc.ac.th>
2025-05-05 20:04:50 +0700

    1st commit

---
+++ first.py
@@ -0,0 +1,6 @@
+myName="chawin"
+myAge=20
+print("My name is", myName, "and I am", myAge, "years old.")
+myName="kewin"
+myAge=40 
+print("My name is", myName, "and I am", myAge, "years old.", sep=" ", end=".\n")   
